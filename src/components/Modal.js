4432ec1d7d8bfd023bd800c81e36135e80671f96
Tsapyk Yelyzaveta <lizatsapik@gmail.com>
2022-03-13 23:31:40 +0200

    Changes in Module view

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,14 +4,16 @@ import './Modal.css';
 function Modal({active, setActive, children, header}) {
 
     return (
-        <div className={active?"Modal active":"Modal"}>
+        <div className={active ? "Modal active" : "Modal"}>
             <div className="Modal-content">
                 <div className="Modal-content-header">
                     <h3 className="Modal-header">{header}</h3>
                     <button className="close-button" onClick={() => setActive(false)}>🞪</button>
                 </div>
+                <div className="Modal-content-space">
+                    {children}
+                </div>
 
-                {children}
             </div>
         </div>
     );
